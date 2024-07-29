92ac512081fddbdb83a9f1968544a870a8f9b897
Namani31 <kwg531@naver.com>
2024-07-29 14:18:19 +0900

    Feat: 상세페이지, 소개페이지 추가

--- src/step05/App.js
+++ src/step05/App.js
@@ -1,12 +1,19 @@
 // 3. Router 적용
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Detail } from './pages/Detail';
 
 function App() {
-  
   return (
-    <div>
-      {isLoading ? `Loading... ${loadCounter}`: displayMovies() }
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/detail" element={<Detail/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/step05/pages/Detail.jsx
@@ -0,0 +1,8 @@
+import './Detail.css';
+export function Detail() {
+    return (
+        <div className='nav'>
+            <span>상세 페이지</span>
+        </div>
+    );
+}
