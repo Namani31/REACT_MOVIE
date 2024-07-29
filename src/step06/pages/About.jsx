003c0519dd21f967168d04efce0a4f68fa4a634f
Namani31 <kwg531@naver.com>
2024-07-29 14:39:18 +0900

    Feat: `About` 페이지에 CSS 적용

---
+++ src/step06/pages/About.jsx
@@ -0,0 +1,9 @@
+import './About.css';
+export function About() {
+    return (
+        <div className='about_container'>
+            <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur quasi esse corporis possimus necessitatibus maxime perspiciatis voluptas dolore nesciunt, totam fuga suscipit, sed quia assumenda beatae tempora ipsam autem facere?</span>
+            <span>영화 리뷰 사이트, 2024</span>
+        </div>
+    );
+}
